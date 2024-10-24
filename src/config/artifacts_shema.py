4b155da9647090d9eba110fb89ab18c8cc227f3b
trishaldoshi <trishaldoshi@gmail.com>
2024-10-24 18:14:25 +0800

    artifacts schema

--- src/config/artifacts_shema.py
+++ src/config/artifacts_shema.py
@@ -4,3 +4,15 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     train_file_path:str
     test_file_path:str
+
+@dataclass
+class DataValidationArtifact:
+    pass
+
+@dataclass
+class DataTransformationArtifact:
+    encoded_file_path:str
+
+@dataclass
+class TrainingModelArtifact:
+    pass
